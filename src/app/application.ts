97c5345b08252e0f8f894233353643a72a0c8cd0
YanaRyazanova <yanaryazanovs@gmaul.com>
2022-10-16 18:09:39 +0500

    3.8. Архитектура приложения

---
+++ src/app/application.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import {inject, injectable} from 'inversify';
+import {ILogger} from '../common/logger/logger-interface.js';
+import {IConfig} from '../common/config/config-interface.js';
+import {Component} from '../models/component.js'
+
+@injectable()
+export default class Application {
+  constructor(
+    @inject(Component.LoggerInterface) private logger: ILogger,
+    @inject(Component.ConfigInterface) private config: IConfig) {}
+
+  public async init() {
+    this.logger.info('Application initialization…');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+  }
+}
